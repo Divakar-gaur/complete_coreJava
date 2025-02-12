d5b265b7a5f948061ba53be3289c2ef8ff6b0787
Divakar-gaur <divakar.gaur01@gmail.com>
2025-02-13 01:34:18 +0530

    correction in class name and code

--- src/Strings/ReverseString.java
+++ src/Strings/ReverseString.java
@@ -1,13 +1,15 @@
 package Strings;
 
+import java.sql.SQLOutput;
+
 public class ReverseString {
     public static void main(String[] args) {
-        String s="pratwish";
+        String s="PRATWISH";
         String res="";
-        for (int i=s.length();i<=0;i++){
-            res+=s.charAt(i);
+        for(int i=s.length()-1; i>=0;i--){
+            res=res+s.charAt(i);
         }
-        System.out.println("Original String "+s);
-        System.out.println("Reversed String "+res);
+        System.out.println("original string:- " + s);
+        System.out.println("reverse string:- " + res);
     }
 }
---
+++ src/Strings/basic.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class basic {
+    public static void main(String[] args) {
+        String s="JSpiders";
+        System.out.println("length is "+s.length());
+        System.out.println("character at third place is "+s.charAt(2));
+        //System.out.println(s.charAt(10)); //StringIndexOutOfBoundsException
+    }
+}
